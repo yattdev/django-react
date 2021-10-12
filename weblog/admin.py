33823e63873fece1a824e50e1f835f860c2b70b7
yattdev <yattdeveloper@gmail.com>
2021-10-12 17:45:30 +0100

    Registrer models to admin

--- weblog/admin.py
+++ weblog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from weblog import models as m
 
 # Register your models here.
+
+admin.site.register(m.Categorie)
+admin.site.register(m.Author)
+admin.site.register(m.Post)
